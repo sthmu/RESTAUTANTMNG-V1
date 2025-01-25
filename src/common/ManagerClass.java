b58c99ecfc3adc642dcbb95ceed4a1cbc5aade88
testingSthm <padparadschaman@gmail.com>
2025-01-25 11:34:43 +0530

    clear console method defined

--- src/common/ManagerClass.java
+++ src/common/ManagerClass.java
@@ -1,6 +1,17 @@
 package common;
 
 public class ManagerClass {
-
+    public static void clearConsole() {
+        try {
+            if (System.getProperty("os.name").contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+            }
+        } catch (Exception e) {
+            System.out.println("Error clearing console.");
+        }
+    }
 
 }
